545b213efecb6a143ef7752b3395b377ccf7d8ae
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2023-11-22 08:00:37 +0100

    added live config

--- manager/includes/functions/db_config.php
+++ manager/includes/functions/db_config.php
@@ -3,9 +3,13 @@ session_start();
 ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../session'));
 
 define("HOST", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
-define("DBNAME", "nimble");
+define("USERNAME", "euanchordispatch");
+define("PASSWORD", "euanchordispatch");
+define("DBNAME", "anamcrke_euanchordispatch");
+// define("HOST", "localhost");
+// define("USERNAME", "root");
+// define("PASSWORD", "");
+// define("DBNAME", "nimble");
 
 $link = mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME);
 
