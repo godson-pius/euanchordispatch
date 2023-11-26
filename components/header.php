26b30b3eeccf20cad8ba40dfb85b03207a71485c
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2023-11-26 15:47:02 +0100

    ADDED SOME SEO

--- components/header.php
+++ components/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="EuanchorDispatch provides full ocean freight service to move your commercial and project cargo all over the world. We leverage our long-standing relationships with our carrier partners to provide you with efficient and economical solutions for all your shipping needs.">
+    <meta name="keywords" content="EuanchorDispatch, Dispatch, Logistics, Delivery, Ocean Freight, Air Freight, Rail Freight, Road Freight">
     <title>Euanchordispatch Logistics</title>
     <!-- favicon -->
     <link rel=icon href=assets/img/favicon.png sizes="20x20" type="image/png">
